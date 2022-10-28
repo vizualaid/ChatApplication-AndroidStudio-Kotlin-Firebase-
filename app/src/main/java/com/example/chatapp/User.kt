58a0f3541a5d6cd078801b262af75cfd0f55c2bc
vizualaid <isha.12011399@lpu.in>
2022-10-28 19:26:38 +0530

    loginAndSignup

---
+++ app/src/main/java/com/example/chatapp/User.kt
@@ -0,0 +1,13 @@
+package com.example.chatapp
+
+//import android.provider.ContactsContract.CommonDataKinds.Email
+
+class User//   constructor(){}
+//constructor(name:String?,email: String?,uid: String?)
+//{  this.name = name
+//    this.email = email
+//    this.uid = uid
+//}
+    (var name: String?, var email: String?, var uid: String?) {
+
+}
